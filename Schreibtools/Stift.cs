6f28474647e4c5fb6511eb38e87bc0219cebde4b
LukasSemler <semler.l04@htlwienwest.at>
2020-10-14 12:00:13 +0200

    Projektdateien hinzufügen.

---
+++ Schreibtools/Stift.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schreibtools
+{
+    class Stift
+    {
+        //Prop
+        public int Strichstärke { get; set; }
+        public ConsoleColor Farbe { get; set; }
+        public string Typ { get; set; }
+
+
+        //Konstruktor
+
+        public Stift(int strichstärke)
+        {
+            Strichstärke = strichstärke;
+            Typ = "unbekannt";
+            Farbe = ConsoleColor.Black;
+               
+        }
+
+        public Stift(int strichstärke, ConsoleColor farbe) : this(strichstärke)
+        {
+            Farbe = farbe;
+        }
+
+        public abstract string void SchreibeText(string text);
+        
+    }
+}
