0bc0e60e1383bfc4ed2ac91d2d3b64b866708400
LukasSemler <semler.l04@htlwienwest.at>
2020-10-14 12:31:25 +0200

    Commit 2

--- Schreibtools/Program.cs
+++ Schreibtools/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Schreibtools
 {
@@ -6,7 +7,24 @@ namespace Schreibtools
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Buntstift b1 = new Buntstift(3, ConsoleColor.Green);
+            Buntstift b2 = new Buntstift(4, ConsoleColor.Blue);
+
+            Kugelschreiber k1 = new Kugelschreiber(5);
+            Kugelschreiber k2 = new Kugelschreiber(5);
+
+            List<Stift> liste = new List<Stift>();
+            liste.Add(k1);
+            liste.Add(k2);
+            liste.Add(b1);
+            liste.Add(b2);
+
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item.SchreibeText("Hallo")); 
+            }
+
         }
     }
 }
