71378c7d5128a8e95503ab4c87a81055e2c38bc6
LukasSemler <semler.l04@htlwienwest.at>
2020-10-15 11:10:33 +0200

    Commit 4

--- Schreibtools/Program.cs
+++ Schreibtools/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Schreibtools
 {
@@ -20,12 +21,54 @@ namespace Schreibtools
             liste.Add(b2);
 
 
+            
+
+
+            /*
+             string RandomString(long Length)
+            {
+                System.Random rnd = new System.Random();
+                StringBuilder Temp = new StringBuilder();
+                for (Int64 i = 0; i < Length; i++)
+                {
+                    Temp.Append(Convert.ToChar(((byte)rnd.Next(254))).ToString());
+                }
+                return Temp.ToString();
+            }
+            */
+
             foreach (var item in liste)
             {
+                /*
+                string wörter;
+                for (int i = 0; i < 6; i++)
+                {
+                    Random random = new Random();
+                    int zahl = random.Next(0, 5);
+                    wörter = wordlist[zahl] + " ";
+                }
+                */
                 Console.ForegroundColor = item.Farbe;
-                Console.WriteLine(item.SchreibeText("Hallo")); 
-            }
+                Console.WriteLine($"(Typ: {item.Typ} | Strichstärke: {item.Strichstärke} | Farbe: {item.Farbe}) | Text: {generieren(5)}");
 
+
+
+
+               
+            }
         }
+        public static string generieren(int anzahl)
+        {
+            string wörter = "";
+            string[] wordlist = new string[] { "Baum", "Herbst", "Blätter", "Schnee", "Kürbis" };
+            for (int i = 0; i < anzahl; i++)
+            {
+                Random random = new Random();
+                int zahl = random.Next(0, 5);
+                wörter += wordlist[zahl] + " ";
+            }
+            return wörter;
+        }
+
     }
 }
